a5d6a8d1e827bd1c8197539885c1020f487b0ed4
AppaulPie <133219813+croxcut@users.noreply.github.com>
2026-02-14 02:14:39 +0800

    init: started

---
+++ src/main/java/com/unamed/croxcut/UnamedApplication.java
@@ -0,0 +1,13 @@
+package com.unamed.croxcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnamedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnamedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unamed/croxcut/UnamedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unamed.croxcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnamedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
