41944ce0a7818ef7347fa4f0919ccf5e8f4f324a
AppaulPie <133219813+croxcut@users.noreply.github.com>
2026-02-14 02:20:51 +0800

    fix: correct jdk 17 -> 24

--- src/main/java/com/unamed/croxcut/UnamedApplication.java
+++ src/main/java/com/unamed/croxcut/UnamedApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UnamedApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UnamedApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UnamedApplication.class, args);
+	}
 
 }
